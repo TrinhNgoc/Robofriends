3727ba03c514fd1fda72b6d771b861f88d5975b1
Jenny <jennyngu@hawaii.edu>
2018-09-03 12:06:34 -1000

    Adding Redux Actions, Reducers, Store, and Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />,
+const store = createStore(searchRobots);
+
+ReactDOM.render(<Provider store={store}><App/></Provider>,
 	document.getElementById('root'));
 registerServiceWorker();
